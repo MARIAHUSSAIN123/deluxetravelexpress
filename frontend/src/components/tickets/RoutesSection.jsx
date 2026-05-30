2a4f2f9ee9f4588a277feb7678303fdcb323234a
Maria Hussain <162424488+MARIAHUSSAIN123@users.noreply.github.com>
2026-05-31 01:55:43 +0500

    Change trip time from '09:00 PM' to '09:00 AM'

--- frontend/src/components/tickets/RoutesSection.jsx
+++ frontend/src/components/tickets/RoutesSection.jsx
@@ -42,7 +42,8 @@ const RoutesSection = ({ openSchedules }) => {
         "11:00 AM",
         "01:00 PM",
         "07:00 PM",
-        "09:00 PM",
+        "09:00 AM",
+      ],
       ],
 
       duration: "3h",
