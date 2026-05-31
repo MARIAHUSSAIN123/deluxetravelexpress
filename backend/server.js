7d6e84b6c62b0f216967070616872be4ade77d59
Maria Hussain <162424488+MARIAHUSSAIN123@users.noreply.github.com>
2026-05-31 20:51:33 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -61,7 +61,7 @@ app.use(cors({
   origin: [
     "https://deluxetravelexpress.vercel.app",
     "https://www.deluxetravelexpress.com",
-    "https://deluxeexpresstravel.com",
+    "https://deluxetravelexpress.com",
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5180"
